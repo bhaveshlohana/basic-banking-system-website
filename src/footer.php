eb27e7e9210a1e6943304b97ba530a264eee469d
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-10-20 22:53:34 +0530

    update

---
+++ src/footer.php
@@ -0,0 +1,26 @@
+<section>
+    	<footer class="page-footer grey darken-3">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact Us</h5>
+                <p class="grey-text text-lighten-4">To solve your queries and requests contact us on the following handles</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Social Media Handles</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#">Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#">Instagram</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#">Facebook</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container center">
+            Copyright ©2020 
+            </div>
+          </div>
+        </footer>
+            
+    </section>
---
+++ src/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Banking System</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+
+    <section>
+    	<div class="container">
+        <div class="row">
+            <div class="col s6 description">
+                <p>Lorem ipsum non fugiat qui qui culpa incididunt nulla dolor laboris do id elit voluptate magna sit tempor laborum nostrud ea tempor minim tempor fugiat consequat nulla sunt ullamco ad dolor elit in in non nisi labore sit in cupidatat fugiat qui veniam cillum velit occaecat officia consequat exercitation et voluptate proident sed amet aute ad ad officia dolore ex consectetur tempor non amet magna ea cillum occaecat laborum ut dolor sit duis anim sint dolor amet dolore incididunt enim et nostrud enim eu elit laboris dolor pariatur ex et</p>
+            </div>
+            <div class="col s6">
+                <img src="vector.svg" class="vector" width="500px" height="500px"alt="mobile banking vector image">
+            </div>
+        </div>
+        <div class="go-to">
+        	<a href="transferHistory.php" class="waves-effect waves-light btn-large black z-depth-2">Transfer History</a>
+        	<a href="selectUser.php" class="waves-effect waves-light btn-large black z-depth-2">Select User</a>
+        </div>
+    </div>
+    </section>
+    <?php include('footer.php'); ?>
+</body>
+
+</html>
